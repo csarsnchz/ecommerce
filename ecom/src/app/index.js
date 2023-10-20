e4e2da9c588942b08693d08fbd94f9e296fb30e1
csarsnchz <csarsnchz@gmail.com>
2023-10-20 05:20:59 +0000

    added sass config

---
+++ ecom/src/app/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from 'semantic-ui-react';
+
+export default function Index() {
+    return (
+        <div>
+            <h2>Games Shop</h2>
+            <div>
+                <Button primary>Primary</Button>
+                <Button secondary>Secondary</Button>
+            </div>
+        </div>
+    );
+}
--- ecom/src/app/layout.js
+++ ecom/src/app/layout.js
@@ -1,5 +1,6 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+import "semantic-ui-css/semantic.min.css";
+import "@/app/scss/global.scss";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body className={inter.className}>{children}</body>
     </html>
   )
